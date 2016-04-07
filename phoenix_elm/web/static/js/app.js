56b79d738ef19c9b9789053b2750e2be25aabdcc
gzfrancisco <gz.francisco@gmail.com>
2016-04-07 08:29:42 -0500

    support for mutiple elm modules from the views

--- phoenix_elm/web/static/js/app.js
+++ phoenix_elm/web/static/js/app.js
@@ -20,5 +20,6 @@ import "deps/phoenix_html/web/static/js/phoenix_html"
 
 // import socket from "./socket"
 
-var elmDiv = document.getElementById('elm-main');
-var elmApp = Elm.embed(Elm.SeatSaver, elmDiv);
+var elmDiv = document.getElementById('elmContainer');
+var elmModule = elmDiv.dataset.elmModule;
+var elmApp = Elm.embed(eval(elmModule), elmDiv);
